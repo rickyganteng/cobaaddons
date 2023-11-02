091c360c2e057eb0ed19e67f81aa4f547702504c
syahputraricky <syahputraricky2@gmail.com>
2023-11-02 20:57:03 +0700

    first

--- src/App.js
+++ src/App.js
@@ -3,21 +3,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
+      <div className='video-responsive'>
+        <iframe
+          width='853'
+          height='480'
+          src={`https://www.youtube.com/embed/${embedId}`}
+          frameBorder='0'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          allowFullScreen
+          title='Embedded youtube'
+        />
+      </div>
     </div>
   );
 }
